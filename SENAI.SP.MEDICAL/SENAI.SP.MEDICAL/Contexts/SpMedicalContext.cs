8e1f5d686cea89b721f7c7a61a3e073dd09c2467
unknown <43690999863>
2021-11-25 16:52:12 -0300

    Inicio React

--- SENAI.SP.MEDICAL/SENAI.SP.MEDICAL/Contexts/SpMedicalContext.cs
+++ SENAI.SP.MEDICAL/SENAI.SP.MEDICAL/Contexts/SpMedicalContext.cs
@@ -33,7 +33,7 @@ namespace SENAI.SP.MEDICAL.Contexts
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-QMET0P1\\SQLEXPRESS; initial catalog=SENAI_SP_MEDICAL; user Id=sa; pwd=123enzzo;");
+                optionsBuilder.UseSqlServer("Data Source=NOTE0113C5\\SQLEXPRESS; initial catalog=SENAI_SP_MEDICAL; user Id=sa; pwd=Senai@132;");
             }
         }
 
